b75bd5146fc5391604c36ef0e6f44d46b5498d63
duohongrui <duohongrui@cqnu.edu.cn>
2023-04-05 10:37:08 +0800

    add zinbwaveZinger in ref_data

--- R/simulate_datasets.R
+++ R/simulate_datasets.R
@@ -80,7 +80,7 @@ simulate_datasets <- function(
     if(method[1] == "all"){
       method <- names(all_methods)
     }
-    if(any(c("scDesign", "SPsimSeq", "SCRIP", "zingeR") %in% method)){
+    if(any(c("scDesign", "SPsimSeq", "SCRIP", "zingeR", "zinbwaveZinger") %in% method)){
       ## ref data for these two methods
       if(is.matrix(ref_data)){
         ref_data <- list(ref_data = ref_data)
